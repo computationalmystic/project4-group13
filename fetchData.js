2c1876606830a8295d337dd79398cbfa2bd0a0c8
Trey <treyshaw81@gmail.com>
2019-10-24 10:19:10 -0500

    charts and colors

--- fetchData.js
+++ fetchData.js
@@ -23,8 +23,11 @@ function getRepoGroupInfo() {
                 
                 $("#sectionBody").append('<div id="'+ e.repo_group_id +'"></div>');
                 $("#" + e.repo_group_id).append('<h1>' + e.rg_name + '</h1>');
+                $("#" + e.repo_group_id).append('<canvas id="'+ e.repo_group_id +'-canvas"></canvas>');
 
-                $("body").append("<ul id="+ e.repo_group_id + "></ul>");
+                createGraph(e);
+                
+                /*$("body").append("<ul id="+ e.repo_group_id + "></ul>");
                 
                 e.committers.forEach((f) => {
                     e.totalCommits += f.commits;
@@ -36,10 +39,76 @@ function getRepoGroupInfo() {
                     if (percentage >= 0.1) {
                         $("#" + e.repo_group_id).append("<li>" + f.email + " : " + percentage.toFixed(2) + "%</li>");
                     }
-                });
+                });*/
             });
         });
     });   
+}
+
+function createGraph(data) {
+    var id = data.repo_group_id;
+    var committers = data.committers;
+    var otherCommits = 0; //this accounts for any commit
+    
+    var numbers = [];
+    var emails = [];
+    var bgColors = [];
+    var borderColors = [];
+    
+    committers.forEach(e => { //separate into two arrays
+        
+        if (e.commits > 10) {
+            numbers.push(e.commits);
+            emails.push(e.email);
+            
+            var r = Math.floor(Math.random() * 255);
+            var g = Math.floor(Math.random() * 255);
+            var b = Math.floor(Math.random() * 255);
+            bgColors.push("rgba(" + r + "," + g + "," + b + ", 0.5)");
+            borderColors.push("rgba(" + r + "," + g + "," + b + ", 0.7)");
+        }
+        else {
+            otherCommits += e.commits;
+        }
+        
+    });
+    
+    if(otherCommits > 0){
+        numbers.push(otherCommits);
+        emails.push("Other Contributers");
+    }
+    
+    var config = {
+        type: 'doughnut',
+        data: {
+            datasets: [{
+                borderWidth: 1,
+                data: numbers,
+                label: data.rg_name,
+                backgroundColor: bgColors,
+                borderColor: borderColors
+            }],
+            labels: emails
+        },
+        options: {
+            responsive: true,
+				legend: {
+					display: false,
+				},
+				title: {
+					display: true,
+					text: 'Contributions Per User'
+				},
+				animation: {
+					animateScale: true,
+					animateRotate: true
+				}
+        }
+    };
+    
+    var ctx = document.getElementById(id + "-canvas");
+    window.myChart = new Chart(ctx, config);
+    
 }
 
 /* function getRepoGroupInfo() {
