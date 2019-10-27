1482af53fd9ea1243e9fa0d8ab13a90480fe8536
Jacob Hollis <42551664+Jacob-Hollis@users.noreply.github.com>
2019-10-27 18:40:49 -0500

    Removed https, did not fix problem

--- javascript/fetchData.js
+++ javascript/fetchData.js
@@ -15,12 +15,12 @@ function resetWithFilter() {
 }
 
 function getRepoGroupInfo() {
-    $.get("https://augur.osshealth.io:5000/api/unstable/repo-groups", function(repodata, status){
+    $.get("http://augur.osshealth.io:5000/api/unstable/repo-groups", function(repodata, status){
         if (status == "success") {
             var repoGroups = repodata;
             repoGroups.forEach((e) => {
 
-                $.get("https://augur.osshealth.io:5000/api/unstable/repo-groups/" + e.repo_group_id + "/top-committers?threshold=1", function(committerData, status){
+                $.get("http://augur.osshealth.io:5000/api/unstable/repo-groups/" + e.repo_group_id + "/top-committers?threshold=1", function(committerData, status){
 
                     e.committers = committerData;
                     e.committers.pop();
